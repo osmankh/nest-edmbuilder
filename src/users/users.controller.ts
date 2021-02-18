dabd93c35849b850eef4e5cc2c74d9108591f027
Osman KHODER <osman@merchantwise.com>
2021-02-18 12:52:13 +0200

    Replace Nest Standard Response with express Responses.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,23 @@
-import { Controller, Body, Param, Get, Post } from '@nestjs/common';
+import { Controller, Body, Param, Get, Post, Res, HttpStatus } from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  getAll(): string {
-    return 'All Users returned';
+  getAll(@Res() res: Response) {
+    res.status(HttpStatus.OK).json([]);
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return `user #${id} Returned`;
+  getOne(@Param('id') id: string, @Res() res: Response) {
+    res.status(HttpStatus.OK).json({
+      name: 'osman',
+    });
   }
 
   @Post()
-  async create(@Body() userDto: CreateUserDto) {
-    return 'This action adds a new User';
+  async create(@Body() userDto: CreateUserDto, @Res() res: Response) {
+    res.status(HttpStatus.CREATED).send();
   }
 }
